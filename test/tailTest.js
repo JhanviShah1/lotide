83d29763184f8874012585cff986f93d4ef17ee2
JhanviShah1 <jhanvi.213@gmail.com>
2021-06-30 15:27:05 -0400

    eqArraysTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,8 @@
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('assert');
 const tail = require('../tail');
 
 
 //assertEqual(result.length, 2); // ensure we get back two elements
 //assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 //assertEqual(result[1], "Labs");
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Lighthouse", "Labs");
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
