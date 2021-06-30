2fb0e9461ff47acc4d4da14dd82463dc14cc1087
JhanviShah1 <jhanvi.213@gmail.com>
2021-06-30 13:59:37 -0400

    Refactor lotide

---
+++ test/assertEqualTest.js
@@ -0,0 +1,15 @@
+const assertEqual = require('../assertEqual');
+
+/*const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};*/
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+//module.exports = assertEqual;
