f19e63b94acff758a346f8191a7725fb0ae4a516
JhanviShah1 <jhanvi.213@gmail.com>
2021-07-01 22:12:43 -0400

    lotide

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,5 @@
 const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual')
+const assertArraysEqual = require('../assertArraysEqual');
 
 assertArraysEqual([1,3,4],[5,6,9]);
 assertArraysEqual([1,3,4],[1,3,4]);
